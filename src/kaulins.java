f00fcc96bc8bb4aa7753b1af2353a17dc6dd2ddf
Uvis Uldis Rozentāls <uvis.rozentals@gmail.com>
2022-01-18 10:37:18 +0200

    Ieviesta metode

--- src/kaulins.java
+++ src/kaulins.java
@@ -2,12 +2,16 @@ import java.util.Random;
 
 public class kaulins {
 
+	static void metkaulinu() {
+		Random rand = new Random();
+	int skaitlis = rand.nextInt(6)+1;
+	System.out.println("Uzkrita :"+skaitlis);
+	}
+	
 	public static void main(String[] args) {
 
 	
-		Random rand = new Random();
-skaitlis = rand.nextInt(6)+1;
-System.out.println("Uzkrita :"+skaitlis);
+		metkaulinu();
 		
 	}
 
