6f31c22573f348cdfa0a0d6ba0a3129736a3cbfa
Uvis Uldis Rozentāls <uvis.rozentals@gmail.com>
2022-01-18 10:42:08 +0200

    Met 3 reizes

--- src/kaulins.java
+++ src/kaulins.java
@@ -2,16 +2,18 @@ import java.util.Random;
 
 public class kaulins {
 
-	static void metkaulinu() {
+	static void metkaulinu(int reizes) {
 		Random rand = new Random();
+		for(int i=1; i<=reizes; i++){
 	int skaitlis = rand.nextInt(6)+1;
 	System.out.println("Uzkrita :"+skaitlis);
+	}
 	}
 	
 	public static void main(String[] args) {
 
 	
-		metkaulinu();
+		metkaulinu(3);
 		
 	}
 
