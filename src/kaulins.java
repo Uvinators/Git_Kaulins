972aeb69e9b3e478575a82bc34456d63c1cd93db
Uvis Uldis Rozentāls <uvis.rozentals@gmail.com>
2022-01-18 10:24:11 +0200

    mainits kaulins

--- src/kaulins.java
+++ src/kaulins.java
@@ -4,7 +4,7 @@ public class kaulins {
 
 	public static void main(String[] args) {
 
-		int skaitlis;
+	
 		Random rand = new Random();
 skaitlis = rand.nextInt(6)+1;
 System.out.println("Uzkrita :"+skaitlis);
