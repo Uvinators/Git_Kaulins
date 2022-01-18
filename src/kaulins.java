3de16f2dcfe53e034d4433726585344a9ddaf632
Uvis Uldis Rozentāls <uvis.rozentals@gmail.com>
2022-01-18 11:08:38 +0200

    iespejams ievadit cik reizes mest kaulinu

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,14 +1,21 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class kaulins {
 
 	public static void main(String[] args) {
-
-	
 		Random rand = new Random();
-skaitlis = rand.nextInt(6)+1;
+Scanner scan = new Scanner(System.in);
+	
+System.out.println("cik reizes mest kaulinu?");
+int reizes = scan.nextInt();
+ int skaitlis;
+ for(int i=1; i<=reizes; i++) {
+	 skaitlis = rand.nextInt(6)+1;
+ 
 System.out.println("Uzkrita :"+skaitlis);
 		
 	}
-
+scan.close();
+}
 }
