16a58579c0a61c7f6fa703c3aca125cb0107a18e
Davis Barons <98738242+DaveBXD@users.noreply.github.com>
2022-02-02 11:51:41 +0200

    Update kaulins.java

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,69 +1,76 @@
-import java.util.List;
-import java.util.Random;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class NEzin {
-     static int mestKaulinu(int reizes){
-    	 int skaitlis=0, lielakais=0;
-    	 Random rand = new random();
-    	 for(int i=1; i<reizes; i++){
-    		 skaitlis=rand.nextInt(6)+1;
-    		 System.out.println("Uzkrita skaitlis"+skaitlis);
-    		 if(lielakais<skaitlis){
-    			 lielakais=skaitlis;
-    		 }
-    	 }
-    	 System.out.println("Lielakais skaitlis metienu piegajiena ir "+lielakais);
-    	 return skaitlis;
-     }
+public class Uzdevums {
 	public static void main(String[] args) {
-		int reizes, izvele;
-		List<Integer> pedejie=new ArrayList<Integer>();
-		Scanner scan=new Scanner(System.in);
-		do{
-			System.out.println("1-met kaulinu | 2-Apskatit pedejos | 3-Apturet");
-			izvele=scan.nextInt();
-			switch(izvele){
-			case 1:
-				do {
-					System.out.println("cik reizes met kaulinu");
-					reizes = scan.nextInt();
-				}while(reizes<1);
-				pedejie=mestKaulinu(reizes);
-				break;
-				
-			case 2:
-				System.out.println("pedeja metien skaitli ir?");
-				for(int i=0; i<pedejie.size(); i++){
-					pedejie.get(i);
-					
-				}
-				break;
-				
-			case 3:
-				int lielakais=0;
-				for(int i=0; i<pedejie.size(); i++){
-					if(pedejie.get(i)>lielakais){
-						lielakais = pedejie.get(i);
-					}
-					System.out.println("Lielakais skaitlis pedeja metiena ir"+lielakais);
-					break;
-					
-					case 4:
-					System.out.println("Programma aptureta");
-					break;
-					
-				}
-			}while(izvele!=2);
-			scan.close();
-				}
-	}
-			
+		int studSk, kritSk;
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.#");
+		     
+		do {
+			System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
+			studSk = scan.nextInt();
+		}while(studSk<1);
+		String[] studenti = new String[studSk];
 		
-
-
-
-
+		do {
+			System.out.println("Kāds būs kritēriju skaits?");
+			kritSk = scan.nextInt();
+		}while(kritSk<1);
+		String[] kriteriji = new String[kritSk];
+		int[] kriterijaSvars = new int[kritSk];
+		int[][] kriterijaVertejums = new int[studSk][kritSk];
+		double[] semestraVertejums = new double[studSk];
+		
+		//Ievada studentu vārdus, uzvārdus
+		for(int i=0; i<studenti.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". studentu");
+			studenti[i] = scan.next();
+		}
+		
+		//Definē kritērijus
+		int maxSvars = 100;
+		for(int i=0; i<kriteriji.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". kritēriju");
+			kriteriji[i] = scan.next();
+			//Norāda katra kritērija svaru
+			do {
+				System.out.println("Ievadi "+(i+1)+". kritērija svaru");
+				kriterijaSvars[i] = scan.nextInt();
+			}while(kriterijaSvars[i]>maxSvars || 
+					kriterijaSvars[i]<1 || 
+					(kriterijaSvars[0]==100 && kritSk > 1));
+			maxSvars -= kriterijaSvars[i];
+		}
+		
+		//Norāda vērtējumu kādu ieguvis katrs students par katru kritēriju
+		for(int i=0; i<kriterijaVertejums.length; i++) {
+			for(int j=0; j<kriterijaVertejums[i].length; j++) {
+				do {
+					System.out.println("Ievadi "+studenti[i]+" vērtējumu par kritēriju "+kriteriji[j]);
+					kriterijaVertejums[i][j] = scan.nextInt();
+				}while(kriterijaVertejums[i][j]<0 || kriterijaVertejums[i][j]>10);
+			}
+		}
+		
+		double rezultats;
+		for(int i=0; i<studenti.length; i++) {
+			rezultats=0;
+			for(int j=0; j<kriteriji.length; j++) {
+				rezultats += ((double) kriterijaSvars[j]/100)*kriterijaVertejums[i][j];
+			}
+			semestraVertejums[i] = rezultats;
+		}
+		
+		for(int i=0; i<studenti.length; i++) {	
+			for(int j=0; j<kriteriji.length; j++) {
+				System.out.println("Studenta "+studenti[i]+" vērtējums par kritēriju "+kriteriji[j]+" ir "+kriterijaVertejums[i][j]+", kura svars ir "+kriterijaSvars[j]);
+			}
+			System.out.println("Semestra vērtējums ir "+df.format(semestraVertejums[i])+"\n");
+		}
+		scan.close();
+	}
+}
 
 	
 	
